49c11d1718904eece3ced37f9239d92ebab57878
Sayin <luffyp2001@gmail.com>
2024-09-10 21:11:27 -0500

    Adding Localstorage

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -4,11 +4,12 @@ import { createContext, useEffect, useState, useContext } from "react";
 export const initialState = {theme: "", data: []}
 
 export const ContextGlobal = createContext(undefined);
+const lsFavs = JSON.parse(localStorage.getItem('favs')) || []
 
 export const ContextProvider = ({ children }) => {
   const [theme,setTheme] = useState()
   const [dentists,setDentists] = useState([])
-  const [favs,setFavs] = useState([])
+  const [favs,setFavs] = useState(lsFavs)
 
   const url = 'https://jsonplaceholder.typicode.com/users'
 
@@ -17,6 +18,10 @@ export const ContextProvider = ({ children }) => {
         setDentists(res.data)
       })   
   },[])
+
+  useEffect(()=>{
+    localStorage.setItem('favs', JSON.stringify(favs))
+  }, [favs])
 
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
   return (
